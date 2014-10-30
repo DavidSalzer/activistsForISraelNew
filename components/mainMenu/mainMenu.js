7fc939dcab52885802ca6230656caaca3045c947
shaigiv <gshai@cambium.co.il>
2014-10-30 08:10:18 +0200

    - שינויים בעמוד ראשי

--- components/mainMenu/mainMenu.js
+++ components/mainMenu/mainMenu.js
@@ -18,45 +18,13 @@ socialGroupApp.controller('mainMenu', ['$scope', '$state', 'classAjax', 'general
     $scope.featureDetails = {
         featureName: 'mainMenu',
         featureLogo: "./img/poalim-logo.png",
-        //infoImg: './img/whatsup.png',
         featureColor: 'gray',
         infoHaeder: "מדרג קבלת הניקוד"
 
     };
 
     generalParameters.setFeature($scope.featureDetails);
-    //$scope.swipeCubeWidth = function () { };
-    //window.onload = function () {
-    //    $scope.swipeCube = document.getElementsByClassName('menu-item-feature');
-    //    console.log($scope.swipeCube);
-    //    //var arrFromList = Array.prototype.slice.call($scope.swipeCube);
-    //    //console.log(arrFromList);
-    //    //console.log($scope.swipeCube.length);
-    //    //setTimeout(function () {
-    //    //    $scope.swipeCubeWidth = $scope.swipeCube[1];
-    //    //    console.log($scope.swipeCubeWidth);
-    //    //}, 3000);
-    //    $scope.swipeCubeWidth = function () { console.log('jdfklgj'); return $scope.swipeCube[1].offsetWidth };
-    //    console.log($scope.swipeCubeWidth());
-    //    $scope.$apply();
-    //};
-    //$scope.swipeCube = document.getElementsByClassName('col-sm-6');
-    //console.log($scope.swipeCube);
-    //var arrFromList = Array.prototype.slice.call($scope.swipeCube);
-    //console.log(arrFromList);
-    //console.log($scope.swipeCube.length);
-    //setTimeout(function () {
-    //    $scope.swipeCubeWidth = $scope.swipeCube[1];
-    //    console.log($scope.swipeCubeWidth);
-    //}, 3000);
-    //$scope.swipeCubeWidth = $scope.swipeCube[1];
-    //console.log($scope.swipeCubeWidth);
-
-
-    //$scope.$on('scrollToEnd', function (ngRepeatFinishedEvent) {
-    //    document.getElementById("features-swipe-inner").scrollLeft = ($scope.features.length - 1) * 145;
-    //});
-
+  
 } ])
 
 
