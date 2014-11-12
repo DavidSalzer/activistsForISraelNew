e4bc251bd4b10c5f82d8f3fe53468cf7712115d0
yuvall1 <yyvvll11@gmail.com>
2014-11-12 15:40:34 +0200

    ש

--- components/event/event.js
+++ components/event/event.js
@@ -87,27 +87,33 @@ socialGroupApp.controller('event', ['$rootScope', '$stateParams', '$scope', 'cla
 } ]);
 
 
-socialGroupApp.controller('DatepickerDemoCtrl', ['$scope', 'PostService', function ($scope, PostService) {
 
+
+
+
+socialGroupApp.controller('DatepickerDemoCtrl', ['$scope', 'PostService','$timeout', function ($scope, PostService, $timeout) {
     angular.element(document).ready(function () {
-
+        $scope.monthArray = $scope.$$childHead.$$childHead.rows;
+        $scope.backgroundDateColor($scope.monthArray);
         $scope.$$childHead.toggleMode = null; //prevent select month / year
 
         $scope.$watch(//reset calendar arrow when paging month
 
 			function () { return $scope.$$childHead.$$childHead.rows; },
 			function (newValue) {
-
 			    if (newValue.length > 1) {//a new month was selected
 
 			        $scope.monthArray = newValue; //save month for get back from week display
 			        $scope.togglecaendarArrow(-90); //make sore the arrow is to up 
+                    $scope.backgroundDateColor($scope.monthArray);
 			    }
-			},
-			true
-		);
+                
+			},true);
 
         $scope.$watch('dt', function () {
+            $scope.monthArray = $scope.$$childHead.$$childHead.rows;
+        $scope.backgroundDateColor($scope.monthArray);
+        console.log($scope.monthArray);alert("1");
             console.log('$scope.dt: ');
             console.log($scope.dt);
             $scope.updateFeed($scope.dt);
@@ -164,6 +170,25 @@ socialGroupApp.controller('DatepickerDemoCtrl', ['$scope', 'PostService', functi
         document.getElementById("calendar-arrow").style.setProperty('-o-transform', 'rotate(' + deg + 'deg)');
         document.getElementById("calendar-arrow").style.setProperty('-moz-transform', 'rotate(' + deg + 'deg)');
         document.getElementById("calendar-arrow").style.setProperty('-ms-transform', 'rotate(' + deg + 'deg)');
+    }
+
+    $scope.backgroundDateColor = function (monthArray) {
+        var delay1 = $timeout(function () {var arr = $scope.posts();
+                var d = new Date();
+                
+     for(var i=0; i< arr.length;i++){
+      d.setTime(arr[i].DestinationTime);
+      for (var j = 0; j < $scope.monthArray.length; j++) {
+
+       for (var k = 0; k < $scope.monthArray[j].length; k++) {
+        
+        if(d.getDate() == $scope.monthArray[j][k].date.getDate() && d.getMonth() == $scope.monthArray[j][k].date.getMonth() && d.getFullYear() == $scope.monthArray[j][k].date.getFullYear()){
+                var myNode = document.querySelectorAll('.calendar-area td button');
+                myNode[j*7 + k].style.background = "#B2C8DD";alert("2");
+            }
+       }
+      }
+     } }, 0);
     }
 
 } ]);
